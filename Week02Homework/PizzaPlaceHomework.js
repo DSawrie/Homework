81be1f0e3651d1a64b128a2e94a1a20fb0a562a7
unknown <dylansawrie@gmail.com>
2025-05-13 10:48:11 -0500

    Pizza Homework

---
+++ Week02Homework/PizzaPlaceHomework.js
@@ -0,0 +1,15 @@
+const pizzaplace = "Imo's"; {
+  console.log(typeof pizzaPlace);
+}
+
+let numberOfToppings = 5; {
+  console.log(typeof numberOfToppings);
+}
+
+console.log(`${pizzaplace} offers ${numberOfToppings} pizzatoppings!`)
+
+if (numberOfToppings <= 10) {
+  console.log("Quality, not quantity!");
+  } else {
+    console.log("That's a whole lot of pizza!");
+  }
